7397d2d76e032db772a3ae1551faa0b9dc8d8551
Richard Sweeney <theorobman@gmail.com>
2013-06-09 20:37:44 +0200

    Loads more changes. Great commit message as usual

--- js/app.js
+++ js/app.js
@@ -10,172 +10,172 @@
         interpolate : /\{\{([\s\S]+?)\}\}/g
     }
 
-
     Backbone.pubSub = _.extend( {}, Backbone.Events )
 
-
-    var ResultModel = Backbone.Model.extend({
-        urlRoot: 'http://matapi.se/foodstuff/'
-    })
-
-
-    var ResultView = Backbone.View.extend({
-
-        el: $( '#foodstuff-container' ),
-
-        initialize: function() {
-            this.listenTo( this.model, 'change', this.render );
-            this.model.fetch()
-            Backbone.pubSub.on( 'resultIn', this.emptyResults, this )
-        },
-
-        render: function() {
-            Backbone.pubSub.trigger( 'resultIn' )
-
-            var template = _.template( $( '#foodstuff-template' ).html(), this.model.toJSON() )
-            this.$el.html( template )
-        },
-
-        emptyResults: function() {
-            this.$el.empty()
-        }
-
-    })
-
-
-    var Search = Backbone.Model.extend()
-
-    var SearchCollection = Backbone.Collection.extend({
-
-        model: Search,
-
-        initialize: function( query ) {
-            this.query = query.query
-        },
-
-        url: function() {
-            return 'http://matapi.se/foodstuff/?query=' + this.query
-        }
-
-    })
-
-
-    var SearchView = Backbone.View.extend({
-
-        el: $( '#results-container' ),
-
-        initialize: function( query ) {
-            this.collection = new SearchCollection( query )
-            this.collection.bind( 'reset', this.render, this )
-            this.collection.fetch({ reset: true })
-            Backbone.pubSub.on( 'resultIn', this.emptyResults, this )
-        },
-
-        render: function() {
-            Backbone.pubSub.trigger( 'searchResultsIn' )
-
-            var list = $( '<ul />' ),
-                items = ''
-
-            _.each( this.collection.models, function( model ) {
-                items += _.template( $( '#result-template' ).html(), model.toJSON() )
-            }, this )
-
-            list.append( items )
-            this.$el.html( list )
-        },
-
-        events: {
-            'click a' : 'getFoodStuff'
-        },
-
-        emptyResults: function() {
-            this.$el.empty()
-        },
-
-        getFoodStuff: function( e ) {
-            e.preventDefault()
-            var id = e.target.id
-
-            appRoutes.navigate( '/id=' + id, { trigger: true } )
-
-        }
-
-    })
-
-
-
-
-
-    var AppView = Backbone.View.extend({
-
-        el: $( '#search-container' ),
-
-        initialize: function() {
-            this.render()
-
-            var location = window.location.href.split( '?' )
-            location = location[1].split( '=' )
-
-            switch ( location[0] ) {
-
-                case 'id' :
-                    appRoutes.navigate( '/?id=' + location[1], { trigger: true } )
-                    break
-
-                case 'search' :
-                    appRoutes.navigate( '/?search=' + loaction[1], { trigger: true } )
-                    break
-
-            }
-
-        },
-
-        render: function() {
-            var template = _.template( $( '#search-template' ).html(), {} )
-            this.$el.html( template )
-        },
-
-        events: {
-            'click button' : 'search'
-        },
-
-        search: function( e ) {
-            e.preventDefault()
-
-            var query = encodeURI( $( '#search' ).val() )
-
-            appRoutes.navigate( '/?search=' + query, { trigger: true } )
-
-        }
-
-    })
 
     var AppRoutes = Backbone.Router.extend({
 
         initialize: function() {
-            var appView = new AppView
+            var appView = new Views.App
+            Backbone.history.start({ pushState: true })
         },
 
         routes: {
-            '?search=:query': 'searchFoodstuffs',
-            '?id=:id': 'getFoodstuff'
+            'search/:query': 'searchFoodstuffs',
+            'foodstuff/:id': 'getFoodstuff'
         },
 
         searchFoodstuffs: function( query ) {
-            var searchView = new SearchView({ query: query })
+            var searchView = new Views.Search({ query: query })
+
         },
 
         getFoodstuff: function( id ) {
-            var resultModel = new ResultModel({ id: id }),
-                resultView = new ResultView({ model: resultModel })
+            var resultModel = new Models.FoodStuff({ id: id }),
+                resultView = new Views.FoodStuff({ model: resultModel })
         }
 
     })
 
-    var appRoutes = new AppRoutes
 
-    Backbone.history.start({ pushState: true })
+    var Views = {
+
+        App: Backbone.View.extend({
+
+            el: $( '#search-container' ),
+
+            initialize: function() {
+                this.render()
+            },
+
+            render: function() {
+                var template = _.template( $( '#search-template' ).html(), {} )
+                this.$el.html( template )
+            },
+
+            events: {
+                'submit form' : 'search'
+            },
+
+            search: function( e ) {
+                e.preventDefault()
+                $( '#container' ).addClass( 'loading' )
+
+                var query = encodeURI( $( '#search' ).val() )
+
+                appRoutes.navigate( '/search/' + query, { trigger: true } )
+
+            }
+
+        }),
+
+        FoodStuff: Backbone.View.extend({
+
+            el: $( '#foodstuff-container' ),
+
+            initialize: function() {
+                this.listenTo( this.model, 'change', this.render );
+                this.model.fetch()
+                Backbone.pubSub.on( 'searchResultsIn', this.emptyView, this )
+            },
+
+            render: function() {
+                Backbone.pubSub.trigger( 'resultIn' )
+
+                var template = _.template( $( '#foodstuff-template' ).html(), this.model.toJSON() )
+                this.$el.html( template )
+
+                $( '#container' ).removeClass( 'loading' )
+            },
+
+            emptyView: function() {
+                this.$el.empty()
+            }
+
+        }),
+
+        Search: Backbone.View.extend({
+
+            el: $( '#results-container' ),
+
+            initialize: function( query ) {
+                this.collection = new Collections.Search( query )
+                this.collection.bind( 'reset', this.render, this )
+                this.collection.fetch({ reset: true })
+
+                Backbone.pubSub.on( 'resultIn', this.emptyResults, this )
+            },
+
+            render: function() {
+                Backbone.pubSub.trigger( 'searchResultsIn' )
+
+                var list = $( '<ul />' ),
+                    items = ''
+
+                _.each( this.collection.models, function( model ) {
+                    items += _.template( $( '#result-template' ).html(), model.toJSON() )
+                }, this )
+
+                list.append( items )
+                this.$el.html( list )
+
+                $( '#container' ).removeClass( 'loading' )
+            },
+
+            events: {
+                'click a' : 'getFoodStuff'
+            },
+
+            emptyResults: function() {
+                this.$el.empty()
+            },
+
+            getFoodStuff: function( e ) {
+                e.preventDefault()
+                $( '#container' ).addClass( 'loading' )
+
+                var id = e.target.id
+
+                appRoutes.navigate( '/foodstuff/' + id, { trigger: true } )
+
+            }
+
+        })
+
+    }
+
+
+    var Models = {
+
+        FoodStuff: Backbone.Model.extend({
+            urlRoot: 'http://matapi.se/foodstuff/'
+        }),
+
+        Search: Backbone.Model.extend(),
+
+    }
+
+
+    var Collections = {
+
+        Search: Backbone.Collection.extend({
+
+            model: Models.Search,
+
+            initialize: function( query ) {
+                this.query = query.query
+            },
+
+            url: function() {
+                return 'http://matapi.se/foodstuff/?query=' + this.query
+            }
+
+        })
+
+    }
+
+
+    var appRoutes = new AppRoutes
 
 
 }( Zepto ))
